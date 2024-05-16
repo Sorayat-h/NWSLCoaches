c4b11e3b3021f10b1cd38b3ac571d66db981b762
Soraya the Performance Analyst <167942829+sorayath-performanceanalyst@users.noreply.github.com>
2024-05-16 13:48:19 -0400

    Update NWSL Coaches Analysis.sql

--- NWSL Coaches Analysis.sql
+++ NWSL Coaches Analysis.sql
@@ -1,12 +1,15 @@
-SELECT * 
-FROM `qualified-glow-417301.PortfolioProjects.NWSL_Seasons` 
-;
--- Show the season prior to a coach change after the season and the next 2 seasons
+-- Show the season before a coach change after the season and the next 2 seasons, include calculations for win %, points per game, and position for the next 2 seasons
 with ChangesPost as 
 (
-  SELECT *,
+  SELECT *, round(Points/Games_Played,2) as Points_Per_Game, round((Wins/Games_Played)*100,2) as Win_Percentage,
           LAG(Coach_Change, 1) OVER (Partition by Club ORDER BY Club) AS Season1,
-       LAG(Coach_Change, 2) OVER (Partition by Club ORDER BY Club) AS Season2
+       LAG(Coach_Change, 2) OVER (Partition by Club ORDER BY Club) AS Season2,
+        Lead(Position, 1) OVER (Partition by Club ORDER BY Club) AS Position_Year1,
+        Lead(Position, 2) OVER (Partition by Club ORDER BY Club) AS Position_Year2,
+        Lead(round(Points/Games_Played,2), 1) OVER (Partition by Club ORDER BY Club) AS Points_Per_Year1,
+        Lead(round(Points/Games_Played,2), 2) OVER (Partition by Club ORDER BY Club) AS Points_Per_Year2,
+        Lead(round((Wins/Games_Played)*100,2), 1) OVER (Partition by Club ORDER BY Club) AS Win_Percentage_Year1,
+        Lead(round((Wins/Games_Played)*100,2), 2) OVER (Partition by Club ORDER BY Club) AS Win_Percentage_Year2,
   FROM `qualified-glow-417301.PortfolioProjects.NWSL_Seasons`
   Order by Club ASC, Season ASC
 ) 
